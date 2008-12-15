1b371a8b3d57270cc7fe168e134d358848af94e7
James MacAulay <james@jadedpixel.com>
2008-12-15 00:56:03 -0500

    intersections

--- code/final/lib/packing/space.rb
+++ code/final/lib/packing/space.rb
@@ -1,18 +1,54 @@
 module Packing
+  # Spaces are always n-dimensional with edges that run parallel to the axes,
+  # so no putting items on a diagonal of any kind.
   class Space
+    # :measurements -- an array of one measurement for each axis, e.g. [x,y,z]
+    # :offsets -- an array of each axis' offset from the origin, defaults to zeroes
     def initialize(options={})
       raise ArgumentError, "requires a proper array of :measurements" unless options[:measurements] && options[:measurements].is_a?(Array) && options[:measurements].any? && options[:measurements].all? {|m| m.is_a?(Numeric) && m > 0}
-      raise ArgumentError, ":offsets must be a proper array of offsets" unless options[:offsets].nil? || (options[:offsets].is_a?(Array) && options[:offsets].size == options[:measurements].size && options[:offsets].all? {|o| o.is_a?(Numeric) && o > 0})
-      #raise ArgumentError, ":overlappers must be an array of other spaces" unless options[:overlappers].nil? || options[:overlappers].is_a?(Array) && options[:overlappers].all? {|o| o.is_a?(Space)}
+      @measurements = (@original_measurements = options[:measurements]).dup
       
-      @measurements = @original_measurements = options[:measurements]
+      raise ArgumentError, ":offsets must be a proper array of offsets" unless options[:offsets].nil? || (options[:offsets].is_a?(Array) && options[:offsets].size == arity && options[:offsets].all? {|o| o.is_a?(Numeric) && o >= 0})
       @offsets = options[:offsets] || ([0] * arity)
+      
       @indexes = (0..(arity - 1)).to_a
+      
+      #raise ArgumentError, ":overlappers must be an array of other spaces" unless options[:overlappers].nil? || options[:overlappers].is_a?(Array) && options[:overlappers].all? {|o| o.is_a?(Space)}
       #@overlappers = options[:overlappers] || []
     end
     
     def indexes
       @indexes.dup
+    end
+    
+    def measurements
+      @measurements.dup
+    end
+    
+    def original_measurements
+      @original_measurements.dup
+    end
+    
+    def offsets
+      @offsets.dup
+    end
+    
+    def offsets=(new_offsets)
+      raise ArgumentError, "requires an array of offsets" unless new_offsets.nil? || (new_offsets.is_a?(Array) && new_offsets.size == options[:measurements].size && new_offsets.all? {|o| o.is_a?(Numeric) && o > 0})
+      @offsets = new_offsets
+    end
+    
+    def far_offsets
+      @far_offsets ||= {}
+      @far_offsets[@measurements] ||= @indexes.map {|i| @offsets[i] + @measurements[i]}
+    end
+    
+    def arity
+      @arity ||= @measurements.size
+    end
+    
+    def volume
+      @volume ||= (@measurements.inject(1) {|product,m| product *= m})
     end
     
     # def overlappers
@@ -25,10 +61,6 @@ module Packing
     #   @overlappers += replacements
     #   self
     # end
-    
-    def &(other_space)
-      own_corners_in_other_space
-    end
     
     def corners
       @corners ||= {}
@@ -53,38 +85,22 @@ module Packing
       end
     end
     
-    def far_offsets
-      @far_offsets ||= {}
-      @far_offsets[@measurements] ||= @indexes.map {|i| @offsets[i] + @measurements[i]}
+    def &(other_space)
+      options = {:offsets => [], :measurements => []}
+      indexes.each do |i|
+        line1 = [@offsets[i], far_offsets[i]]
+        line2 = [other_space.offsets[i], other_space.far_offsets[i]]
+        return nil unless intersection = self.class.linear_intersection(line1,line2)
+        options[:offsets] << intersection.first
+        options[:measurements] << intersection.last - intersection.first
+      end
+      Space.new(options)
     end
     
-    def contains_point(point_offsets)
-      
-    end
-    
-    def offsets
-      @offsets.dup
-    end
-    
-    def offsets=(new_offsets)
-      raise ArgumentError, "requires an array of offsets" unless new_offsets.nil? || (new_offsets.is_a?(Array) && new_offsets.size == options[:measurements].size && new_offsets.all? {|o| o.is_a?(Numeric) && o > 0})
-      @offsets = new_offsets
-    end
-    
-    def measurements
-      @measurements.dup
-    end
-    
-    def original_measurements
-      @original_measurements.dup
-    end
-    
-    def arity
-      @arity ||= @measurements.size
-    end
-    
-    def volume
-      @volume ||= (@measurements.inject(1) {|product,m| product *= m})
+    def contains_point?(point_offsets)
+      raise ArgumentError, "point must be a proper array of offsets" unless point_offsets && point_offsets.is_a?(Array) && point_offsets.size == arity && point_offsets.all? {|o| o.is_a?(Numeric) && o >= 0}
+      @indexes.each {|i| return false if point_offsets[i] < offsets[i] || point_offsets[i] > far_offsets[i]}
+      return true
     end
     
     # mapping is an array of indexes
@@ -94,24 +110,57 @@ module Packing
       @measurements = mapping.map {|m| @measurements[m]}
     end
     
-    def reset_rotation
-      @measurements = @original_measurements
-    end
-    
     def rotate_mapping_permutations
       @rotate_mapping_permutations ||= @indexes.permutations
     end
     
-    def can_fit_inside?(other_space)
-      other_measurements = other_space.measurements
-      @measurements.each_with_index do |m,i|
-        return false if other_space.measurements[i] < m
+    def reset_rotation
+      @measurements = @original_measurements
+    end
+    
+    def can_fit_inside?(other_space,options = {})
+      rotatable = (options.has_key?(:rotatable) ? options[:rotatable] : true)
+      other_measurements = (rotatable ? other_space.measurements.sort : other_space.measurements)
+      (rotatable ? @measurements.sort : @measurements).each_with_index do |m,i|
+        return false if other_measurements[i] < m
       end
       return true
     end
     
     def can_contain?(other_space)
       other_space.can_fit_inside?(self)
+    end
+    
+    def self.linear_collision?(line1,line2)
+      lines = setup_linear_comparison(line1,line2)
+      return lines.last.first < lines.first.last
+    end
+    
+    def self.linear_intersection(line1,line2)
+      lines = setup_linear_comparison(line1,line2)
+      return nil unless lines.last.first < lines.first.last
+      near_offset = lines.last.first
+      far_offset = [lines.first.last, lines.last.last].min
+      [near_offset,far_offset]
+    end
+    
+    def ==(other)
+      self.offsets == other.offsets && self.measurements == other.measurements && self.original_measurements == other.original_measurements
+    end
+    
+    def eql?(other)
+      other.is_a?(self.class) && self == other
+    end
+    
+    protected
+    
+    def self.setup_linear_comparison(line1,line2)
+      [line1,line2].each do |arg|
+        unless arg.is_a?(Array) && arg.size == 2 && arg.all?{|n| n >= 0 && n.is_a?(Numeric) } && arg.last >= arg.first
+          raise ArgumentError, "both lines must be a two-element array of valid near and far offsets"
+        end
+      end
+      lines = [line1,line2].sort_by(&:first)
     end
     
   end
--- code/final/test/unit/space_test.rb
+++ code/final/test/unit/space_test.rb
@@ -24,8 +24,53 @@ class SpaceTest < Test::Unit::TestCase
         assert_equal [[1,1,1],[6,1,1],[1,11,1],[6,11,1],[1,1,16],[6,1,16],[1,11,16],[6,11,16],], @space.corners
       end
     end
+
+    context "#indexes" do
+      should "return the correct indexes" do
+        assert_equal [0,1,2], @space.indexes
+      end
+    end
+
+    context "#contains_point?" do
+      should "return false if the space does not contain the point" do
+        assert_equal false, @space.contains_point?([1,1,0])
+        assert_equal false, @space.contains_point?([7,11,16])
+      end
+      should "return true if the space contains the point" do
+        assert_equal true, @space.contains_point?([1,1,1])
+        assert_equal true, @space.contains_point?([6,9,16])
+      end
+    end
+
+    context "#&" do
+      should "return the given space when it is completely contained in self" do
+        contained = Space.new(:measurements => [1,1,1], :offsets => [2,2,2])
+        
+        assert_equal contained, @space & contained
+        assert_equal contained, contained & @space
+      end
+      should "return the correct intersection when there is a partial overlap" do
+        overlapping = Space.new(:measurements => [2,2,2], :offsets => [0,0,0])
+        expected = Space.new(:measurements => [1,1,1], :offsets => [1,1,1])
+        
+        assert_equal expected, @space & overlapping
+        assert_equal expected, overlapping & @space
+      end
+      should "return nil when there is no intersection" do
+        disjointed = Space.new(:measurements => [1,10,10], :offsets => [0,0,0])
+        
+        assert_equal nil, @space & disjointed
+        assert_equal nil, disjointed & @space
+      end
+    end
     
-    
+    context ".linear_intersection" do
+      should "return the correct intersection of two lines represented by two-element arrays of near and far offsets" do
+        assert_equal [1,2], Space.linear_intersection([0,2],[1,3])
+        assert_equal [1,2], Space.linear_intersection([1,3],[0,2])
+        assert_equal [1,3], Space.linear_intersection([1,3],[0,4])
+      end
+    end
     
   end
   
